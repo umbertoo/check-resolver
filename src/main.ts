e3c6cec8f43edfe939e72e66b26735612972f879
VK <v.kokovin@gmail.com>
2020-05-31 17:05:47 +0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,14 @@ import { AppModule, SYMBOL_TOKEN, STRING_TOKEN } from "./app.module";
 async function bootstrap() {
   const app = await NestFactory.createApplicationContext(AppModule);
   try {
-    const factoryProviderWithToken = await app.resolve(SYMBOL_TOKEN);
+    const factoryProviderWithSymbolToken = await app.resolve(SYMBOL_TOKEN);
   } catch (error) {
-    console.log('factoryProviderWithToken', error);
+    console.log('factory provider with SYMBOL_TOKEN', error);
   }
   try {
-    const factoryProviderWithString = await app.resolve(STRING_TOKEN);
+    const factoryProviderWithStringToken = await app.resolve(STRING_TOKEN);
   } catch (error) {
-    console.log('factoryProviderWithString', error);
+    console.log('factory provider with STRING_TOKEN', error);
   }
 }
 bootstrap();
